a97f323bf8bf60e3c5b295b44e949d3edb269feb
nishant <nishant.singh87@gmail.com>
2015-04-30 00:57:18 +0530

    adding demo page for referall app

---
+++ apps/referall/scripts/referall.js
@@ -0,0 +1,12 @@
+(function() {
+  'use strict';
+
+  var init = function () {
+  };
+
+  if(!window.geeksaints) window.geeksaints = {};
+  window.geeksaints.referall = {
+    init: init
+  };
+
+}).call(this);
