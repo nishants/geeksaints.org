1d16e70cc7fbe914de8f8a1669f8609e711680d1
nishant <nishant.singh87@gmail.com>
2015-04-30 12:23:37 +0530

    extracting setup data in router-spec

--- apps/referall/test/specs/router-spec.js
+++ apps/referall/test/specs/router-spec.js
@@ -1,4 +1,6 @@
 var router = {};
+var url = "https:geeksaints.org/referalll#view_jobs_list_all-jobs_location_3";
+var noFragmentsURl = "https:geeksaints.org/referalll";
 
 QUnit.module('Router', {
   setup: function() {
@@ -13,13 +15,15 @@ QUnit.test( "Create Router", function( assert ) {
 });
 
 QUnit.test( "Should resolve view params from url fragment", function( assert ) {
-  var params = router.parseUrl("https:geeksaints.org/referalll#view_jobs_list_all-jobs_location_3");
+  var params = router.parseUrl(url);
+
   assert.equal(params.view, "jobs");
   assert.equal(params.list, "all-jobs");
   assert.equal(params.location, 3);
 });
 
 QUnit.test( "Should return empty params if no url fragment", function( assert ) {
-  var params = router.parseUrl("https:geeksaints.org/referalll");
+  var params = router.parseUrl(noFragmentsURl);
+
   assert.deepEqual(params, {});
 });
