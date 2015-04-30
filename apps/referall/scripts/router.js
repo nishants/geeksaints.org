da47bce85d8359f88e96f788c5b2ff1fbb0fa8d6
nishant <nishant.singh87@gmail.com>
2015-04-30 12:21:15 +0530

    adding Router.parseUrl

--- apps/referall/scripts/router.js
+++ apps/referall/scripts/router.js
@@ -1,12 +1,26 @@
-
-(function() {
+(function () {
   'use strict';
+  //url format : uri#param-name-1_param-value-1_param-name2_param-value2
+  var Router = function (state) {
+  };
+  var hasFragment = function (url) {
+    return url.indexOf("#") != -1;
+  };
 
-  var Router = function(state){};
+  var fragmentFrom = function (url) {
+    return url.split("#")[1];
+  };
 
   // return view params from url
-  Router.prototype.parseUrl = function () {
-
+  Router.prototype.parseUrl = function (url) {
+    var params = {};
+    if (hasFragment(url)) {
+      var tokens = fragmentFrom(url).split("_");
+      for (var i = 0; i < tokens.length - 1; i += 2) {
+        params[tokens[i]] = tokens[i + 1];
+      }
+    }
+    return params;
   };
 
   // update route on view navigation
--- apps/referall/test/specs/router-spec.js
+++ apps/referall/test/specs/router-spec.js
@@ -1,4 +1,3 @@
-
 var router = {};
 
 QUnit.module('Router', {
@@ -9,6 +8,18 @@ QUnit.module('Router', {
   }
 });
 
-QUnit.test( "Router", function( assert ) {
-  assert.ok( router, "Router found!" );
+QUnit.test( "Create Router", function( assert ) {
+  assert.ok( router, "Cannot create router" );
+});
+
+QUnit.test( "Should resolve view params from url fragment", function( assert ) {
+  var params = router.parseUrl("https:geeksaints.org/referalll#view_jobs_list_all-jobs_location_3");
+  assert.equal(params.view, "jobs");
+  assert.equal(params.list, "all-jobs");
+  assert.equal(params.location, 3);
+});
+
+QUnit.test( "Should return empty params if no url fragment", function( assert ) {
+  var params = router.parseUrl("https:geeksaints.org/referalll");
+  assert.deepEqual(params, {});
 });
