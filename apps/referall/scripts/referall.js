16355b51d6cc068376cf955174e4cbfc4e247fe2
nishant <nishant.singh87@gmail.com>
2015-05-19 23:40:36 +0530

    some random change.

--- apps/referall/scripts/referall.js
+++ apps/referall/scripts/referall.js
@@ -1,12 +1,29 @@
 (function() {
   'use strict';
 
-  var render = function () {
+  var referall = {};
+  referall.refer = function(jobNumber){
+    var response_type = "code",
+        client_id = "75bhjbeib8ljx4",
+        redirect_uri = "http://www.geeksaints.org/apps/referall",
+        state = "view_refer-job_job-id-"+jobNumber+"_origin-linkedin",
+        scope = "r_network r_basicprofile r_contactinfo r_emailaddress r_fullprofile r_network rw_company_admin rw_groups rw_nus w_messages w_share";
 
+    redirect_uri = redirect_uri
+    redirect_uri = encodeURI(redirect_uri);
+    scope = encodeURI(scope);
+    redirect_uri = encodeURI(redirect_uri);
+    state = encodeURI(state);
+
+    var url = "https://www.linkedin.com/uas/oauth2/authorization?"
+            + "response_type=" + response_type
+            + "&client_id=" + client_id
+            + "&redirect_uri=" + redirect_uri
+            + "&state=" + state
+            + "&scope=" +scope
+        ;
+    window.location.href = url;
   };
 
-  var referall = {};
-  referall.state = {};
-  referall.render = render;
   window.referall = referall
 }).call(this);
