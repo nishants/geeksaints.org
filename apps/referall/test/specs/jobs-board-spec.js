b691d6ac5018fcf060cc27637c5d0663eb8fd972
nishant <nishant.singh87@gmail.com>
2015-05-20 16:43:34 +0530

    adding test for filter by location.

--- apps/referall/test/specs/jobs-board-spec.js
+++ apps/referall/test/specs/jobs-board-spec.js
@@ -1,40 +1,69 @@
 (function () {
   "use strict"
 
-  var jobs = [{url:"1"},{url:"2"},{url:"3"}],
+  var
+      mumbaiDeveloperFlipkart = {url: "1", location: "Mumbai", role: "Developer", emlpoyer: "flipkart"},
+      delhiTesterAmazon = {url: "2", location: "Delhi", role: "Tester", emlpoyer: "amazon"},
+      bangaloreManagerSnapdeal = {url: "3", location: "Bangalaore", role: "Manager", emlpoyer: "snapdeal"},
+      delhiTesterFlipkart = {url: "4", location: "Delhi", role: "Tester", emlpoyer: "flipkart"},
+
+      jobs = [
+        mumbaiDeveloperFlipkart,delhiTesterAmazon, bangaloreManagerSnapdeal, delhiTesterFlipkart
+      ],
+
       jobsCreated = [],
+
       MockedJobsCard = function (job) {
         this.appendTo = function($e){
           this.appendedTo = $e;
           jobsCreated.push(job);
         };
 
-        this.assertAppendedTo = function(assert, toPage){
-          var appendedTo = this.appendedTo;
-          assert.equal(toPage, appendedTo, "Jobs Card must be appended to the page")
+        this.remove = function(){
+          jobsCreated.splice(jobsCreated.indexOf(job), 1);
         };
       },
       page = function(){return $("#jobs-board");},
       jobCards = function(){return $(".job-card:visible");},
+
+      filterByLocation = function(location){
+        var input = $("#filter-by-location");
+        input.val(location);
+        input.trigger("blur");
+      },
+
       pageHtml =
-          '<div id="jobs-board">                            '+
-          '   <div id="jobs-list" class="jobs-list">        '+
-          '     <div id="sample-job-card" class="job-card">      '+
-          '   </div>                                        '+
-          '</div>                                           ';
+          '<div id="jobs-board">                                      '+
+          '   <div id="filter-jobs" class="menu">                     '+
+          '       <input type="text" id="filter-by-location"></div>   '+
+          '       <input type="text" id="filter-by-employer"></div>   '+
+          '       <input type="text" id="filter-by-role"></div>       '+
+          '   </div>                                                  '+
+          '   <div id="jobs-list" class="jobs-list">                  '+
+          '     <div id="sample-job-card" class="job-card">           '+
+          '   </div>                                                  '+
+          '</div>                                                     ';
 
   QUnit.module("JobsBoard", {
     setup: function () {
       $("#qunit-fixture").append(pageHtml);
+      page().hide();
+      jobsCreated = []
+      new referall.JobsBoard(page(), jobs, MockedJobsCard);
     }
   });
 
   QUnit.test("Should Display Page", function (assert) {
-    page().hide();
-    new referall.JobsBoard(page(), jobs, MockedJobsCard);
-
     assert.ok(page().is(":visible"), "Should display page");
     assert.deepEqual(jobsCreated, jobs, 'Should create a JobsCard for each jobs and append to document')
+  });
+
+  QUnit.test("Should filter jobs by location, employer or role", function (assert) {
+    var delhiJobs = [delhiTesterAmazon, delhiTesterFlipkart];
+
+    filterByLocation("Delhi");
+
+    assert.deepEqual(jobsCreated, delhiJobs , 'Should filter by location')
   });
 
 }).call(this);
